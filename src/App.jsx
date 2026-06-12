454972508f146762c59cbe5c6a91c353dc5fe30f
FelipeAcuna-hub <felipe.acuna2@mail.udp.cl>
2026-06-12 00:17:33 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { supabase } from './supabaseClient';
 
-import Layout from './components/layout2'; 
+import Layout from './components/layout'; 
 
 // Importación de tus páginas
 import Login from './pages/Login';
